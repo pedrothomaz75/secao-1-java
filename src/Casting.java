426767af1af82b8c0d440dcb207029c28ccb9a69
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-16 19:32:18 -0300

    feat: added casting Class

---
+++ src/Casting.java
@@ -0,0 +1,13 @@
+public class Casting {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        // O casting é basicamente uma forma de conversão de tipos primtiivos
+        resultado = (double) a /b;
+        System.out.println(resultado);
+    }
+}
